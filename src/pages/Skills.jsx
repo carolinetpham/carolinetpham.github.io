bcb724a92853bfc5a5738b8a90bdbadfa5bea722
carolinetpham <carolinetpham@outlook.com>
2024-03-25 16:06:48 -0400

    adding styling to skills page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -4,20 +4,20 @@ import React from 'react';
 export default function Skills() {
     return (
         <div id="skills">
-            <div className='skills-header'>
+            <div className='skills-title'>
                 <h1>Skills</h1>
             </div>
-            <div className='languages-containter'>
-                <h1>Languages:</h1>
-                <p>Java, JavaScript, HTML, CSS</p>
+            <div className='languages'>
+                <h1 className='languages-header'>Languages:</h1>
+                <p className='languages-desc'>Java, JavaScript, HTML, CSS</p>
             </div>
-            <div className='tools-container'>
-                <h1>Tools:</h1>
-                <p>Figma, Gradle, Git, GitHub, VS Code, Eclipse, IntelliJ, Photoshop, Illustrator</p>
+            <div className='tools'>
+                <h1 className='tools-header'>Tools:</h1>
+                <p className='tools-desc'>Figma, Gradle, Git, GitHub, VS Code, Eclipse, IntelliJ, Photoshop, Illustrator</p>
             </div>
-            <div className='lib-fw-container'>
-                <h1>Libraries & Frameworks:</h1>
-                <p>React, Bootstrap, JavaFX</p>
+            <div className='lib-fw'>
+                <h1 className='lib-fw-header'>Libraries & Frameworks:</h1>
+                <p className='lib-fw-desc'>React, Bootstrap, JavaFX</p>
             </div>
         </div>
     )
