bf58a1c72c785fcfb6ea52d706e15fd433ef0c59
carolinetpham <carolinetpham@outlook.com>
2024-03-25 16:19:16 -0400

    adding @media style for skills

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
         <div id='home'>
             <div id='home-container'>
                 <div id='home-intro'>
-                    <h1 className='name'>Hi! I'm Caroline!</h1>
+                    <h1 className='name'>Hi! I'm Caroline :)</h1>
                     <h2 className='major'>Candidate for B.S. in Computer Science and Design</h2>
                     <div>
                         <SocialMediaIcons />
