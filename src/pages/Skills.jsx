db6a1bff4a52770f4e0deb9756d740bbccb56c33
carolinetpham <carolinetpham@outlook.com>
2024-03-25 15:52:59 -0400

    fixed navigation bar margins

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -4,13 +4,21 @@ import React from 'react';
 export default function Skills() {
     return (
         <div id="skills">
-            <h1>Skills</h1>
-            <h3>Languages:</h3>
-            <p>Java, JavaScript, HTML, CSS</p>
-            <h3>Tools:</h3>
-            <p>Git, Figma, Illustrator, Photoshop, Visual Studio Code</p>
-            <h3>Frameworks:</h3>
-            <p>React, Bootstrap</p>
+            <div className='skills-header'>
+                <h1>Skills</h1>
+            </div>
+            <div className='languages-containter'>
+                <h1>Languages:</h1>
+                <p>Java, JavaScript, HTML, CSS</p>
+            </div>
+            <div className='tools-container'>
+                <h1>Tools:</h1>
+                <p>Figma, Gradle, Git, GitHub, VS Code, Eclipse, IntelliJ, Photoshop, Illustrator</p>
+            </div>
+            <div className='lib-fw-container'>
+                <h1>Libraries & Frameworks:</h1>
+                <p>React, Bootstrap, JavaFX</p>
+            </div>
         </div>
     )
 }
