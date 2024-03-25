4b16270306b4241f487c445194954fbcc2e27e46
carolinetpham <carolinetpham@outlook.com>
2024-03-25 09:27:56 -0400

    adjusted style for Navbar and home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,14 @@ import SocialMediaIcons from '../components/SocialMediaIcons';
 
 export default function Home() {
     return (
-        <div id="home">
-            <div id="home-intro">
-                <h1>Hi! I'm Caroline!</h1>
-                <h2>Candidate for B.S. in Computer Science and Design</h2>
-                <div>
-                    <SocialMediaIcons />
+        <div id='home'>
+            <div id='home-container'>
+                <div id='home-intro'>
+                    <h1 className='name'>Hi! I'm Caroline!</h1>
+                    <h2 className='major'>Candidate for B.S. in Computer Science and Design</h2>
+                    <div>
+                        <SocialMediaIcons />
+                    </div>
                 </div>
             </div>
         </div>
