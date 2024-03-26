edd62d6139a5531ba43915afaad2086294660786
carolinetpham <carolinetpham@outlook.com>
2024-03-26 16:29:00 -0400

    removing experience section while I work on it

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Skills from './pages/Skills';
 import Projects from './pages/Projects';
-import Experience from './pages/Experience';
+// import Experience from './pages/Experience';
 
 function App() {
   return (
