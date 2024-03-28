9fed9c992d17bbeea55b74248fda9dedeea31d0b
carolinetpham <carolinetpham@outlook.com>
2024-03-27 20:25:10 -0400

    adding some comments

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -1,3 +1,4 @@
+// src/components/SocialMediaIcons.jsx
 import React from 'react';
 
 export default function SocialMediaIcons() {
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,3 +1,4 @@
+// src/components/Timeline.jsx
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
