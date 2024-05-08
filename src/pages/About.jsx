ce4b4f47c507780b6fa134634166269928ba27b7
carolinetpham <carolinetpham@outlook.com>
2024-05-08 16:33:47 -0400

    updating about me description

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,16 +11,12 @@ export default function About() {
             <div className='about-container'>
                 <Image src={'/pictures/headshot.png'} alt='Caroline Pham Picture' className='profile-picture' />
                 <div className='intro-resume'>
-                    <p className='introduction'>I'm a second-year Computer Science and Design student at
-                        Northeastern University with a strong passion for all things
-                        front-end and design. Currently, I'm focusing my studies on
-                        software development, and I've had the privilege of honing
-                        my programming skills in Java through my coursework.
-                        Alongside my academic pursuits, I'm a self-taught enthusiast
-                        in creative tools like Figma and Illustrator. I find immense joy
-                        in blending my technical knowledge with a keen eye for
-                        design, bringing a unique perspective to the world of
-                        software development.</p>
+                    <p className='introduction'>I'm a rising third-year majoring in Computer Science and Design student at
+                        Northeastern University with a strong passion for front-end development and UI/UX design.
+                        Currently, I'm focusing my studies on software development, and I've had the privilege of
+                        honing my programming skills in Java through my coursework. Alongside my academic pursuits,
+                        I'm a self-taught enthusiast in creative tools like Figma and React. I find immense joy in blending my
+                        technical knowledge with a keen eye for design, bringing a unique perspective to the world of software development.</p>
                     <a href='https://drive.google.com/file/d/1oiF1Wu4GbbR3Vy2QILjLjtTgMXqTobDO/view?usp=sharing'
                         target='_blank' rel='noreferrer' class='button-style'>Resume</a>
                 </div>
