cead85e825e0772ebad0b61e34a0abb38005899b
carolinetpham <carolinetpham@outlook.com>
2024-05-15 19:18:40 -0400

    updating about me description

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,7 +15,7 @@ export default function About() {
                         Northeastern University with a strong passion for front-end development and UI/UX design.
                         Currently, I'm focusing my studies on software development, and I've had the privilege of
                         honing my programming skills in Java through my coursework. Alongside my academic pursuits,
-                        I'm a self-taught enthusiast in creative tools like Figma and React. I find immense joy in blending my
+                        I'm a self-taught enthusiast in creative tools like Figma and illustrator and front-end frameworks like React and Bootstrap. I find immense joy in blending my
                         technical knowledge with a keen eye for design, bringing a unique perspective to the world of software development.</p>
                     <a href='https://drive.google.com/file/d/1oiF1Wu4GbbR3Vy2QILjLjtTgMXqTobDO/view?usp=sharing'
                         target='_blank' rel='noreferrer' class='button-style'>Resume</a>
